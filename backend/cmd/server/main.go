ee0ef967d0a4976d7ba13ee484a75a20ac426b34
Ethan Myos <56983817+etanetan@users.noreply.github.com>
2025-02-04 18:10:24 -0600

    Update main.go

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -12,7 +12,7 @@ func withCORS(h http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, ngrok-skip-browser-warning")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
         // If it's an OPTIONS request, we can stop here.
         if r.Method == http.MethodOptions {
             w.WriteHeader(http.StatusOK)
